e938139d0f04578025435cd17c412e006e6b6ca9
its-kumar <kumarshanu1009@gmail.com>
2020-01-16 22:00:20 +0530

    updating

---
+++ Part 3 - Classification/Naive Bayes Classification.R
@@ -0,0 +1,73 @@
+# Naive Bayes Classifier
+
+# Importing the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[, 3:5]
+
+### ============ Important =============== ###
+# Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0,1))
+
+
+# Splitting dataset into training set and test set
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature scaling
+training_set[, 1:2] = scale(training_set[, 1:2])
+test_set[, 1:2] = scale(test_set[, 1:2])
+
+
+# create  classifeir and fit
+library(e1071)
+classifier = naiveBayes(x = training_set[-3],
+                        y = training_set$Purchased)
+
+
+# Predicting the test set
+y_pred = predict(classifier, newdata = test_set[-3])
+
+# Making the confusion matrix
+cm = table(test_set[, 3], y_pred)
+
+# Visualise the training set results
+#install.packages('ElemStatLearn')
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1] -1), max(set[, 1] + 1), by=0.01)
+X2 = seq(min(set[, 2] -1), max(set[, 2] + 1), by=0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, 3],
+     main = "Naive Bayes Classifeir (Training Set)",
+     xlab = "Age", ylab="Estimated Salary", 
+     xlim=range(X1), ylim = range(X2))
+
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch='.', col=ifelse(y_grid ==1, 'springgreen3', 'tomato') )
+points(set, pch=21, bg=ifelse(set[, 3] ==1, 'green4', 'red3'))
+
+# Visualise the test set results
+#install.packages('ElemStatLearn')
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1] -1), max(set[, 1] + 1), by=0.01)
+X2 = seq(min(set[, 2] -1), max(set[, 2] + 1), by=0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier,
+                 newdata = grid_set
+)
+plot(set[, 3],
+     main = "Naive Bayes Classifier (Test Set)",
+     xlab = "Age", ylab="Estimated Salary", 
+     xlim=range(X1), ylim = range(X2))
+
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch='.', col=ifelse(y_grid ==1, 'springgreen3', 'tomato') )
+points(set, pch=21, bg=ifelse(set[, 3] ==1, 'green4', 'red3'))
+
--- Part 3 - Classification/Random Forest Classification.R
+++ Part 3 - Classification/Random Forest Classification.R
@@ -1,8 +1,12 @@
-# Kernel - SVM 
+# Random Forest Classification
 
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
 dataset = dataset[, 3:5]
+
+### ============ Important =============== ###
+# Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0,1))
 
 # Splitting dataset into training set and test set
 library(caTools)
@@ -12,17 +16,16 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature scaling
-training_set[, 1:2] = scale(training_set[, 1:2])
-test_set[, 1:2] = scale(test_set[, 1:2])
+training_set[, -3] = scale(training_set[, -3])
+test_set[, -3] = scale(test_set[, -3])
 
 
-# fitting classifier to training set
-# install.packages('e1071')
-library(e1071)
-classifier = svm(formula = Purchased ~ ., 
-                 data = training_set,
-                 type = 'C-classification',
-                 kernel = 'radial')
+# create your Random Forest Classification here
+#install.packages('randomForest')
+library(randomForest)
+classifier = randomForest(x = training_set[-3], 
+													y = training_set$Purchased,
+													ntree = 10)
 
 # Predicting the test set
 y_pred = predict(classifier, newdata = test_set[-3])
@@ -39,13 +42,13 @@ X2 = seq(min(set[, 2] -1), max(set[, 2] + 1), by=0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier,
-                 type = "response",
-                 newdata = grid_set
+								 type = "response",
+								 newdata = grid_set
 )
 plot(set[, 3],
-     main = "SVM Classifeir (Training Set)",
-     xlab = "Age", ylab="Estimated Salary", 
-     xlim=range(X1), ylim = range(X2))
+		 main = "Random Forest Classification(Training Set)",
+		 xlab = "Age", ylab="Estimated Salary", 
+		 xlim=range(X1), ylim = range(X2))
 
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch='.', col=ifelse(y_grid ==1, 'springgreen3', 'tomato') )
@@ -60,13 +63,13 @@ X2 = seq(min(set[, 2] -1), max(set[, 2] + 1), by=0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier,
-                 type = "response",
-                 newdata = grid_set
+								 type = "response",
+								 newdata = grid_set
 )
 plot(set[, 3],
-     main = " SVM (Test Set)",
-     xlab = "Age", ylab="Estimated Salary", 
-     xlim=range(X1), ylim = range(X2))
+		 main = " Random Forest Classification (Test Set)",
+		 xlab = "Age", ylab="Estimated Salary", 
+		 xlim=range(X1), ylim = range(X2))
 
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch='.', col=ifelse(y_grid ==1, 'springgreen3', 'tomato') )
