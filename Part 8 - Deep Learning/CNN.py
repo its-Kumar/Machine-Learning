c4bfbc2d7f84a5b206a8be696d6f91376b36d5f8
its-kumar <kumarshanu1009@gmail.com>
2020-01-26 13:01:18 +0530

    Update CNN.py

--- Part 8 - Deep Learning/CNN.py
+++ Part 8 - Deep Learning/CNN.py
@@ -1,3 +1,17 @@
 # Convolutional Neural Network
 
-# Part 1 -
+# Part 1 -Building the CNN
+
+# Importing the keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense
+
+# Intialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(filters=32, kernel_size=(3, 3),
+                             input_shape=(64, 64, 3), activation='relu'))
+
+# Step 2 - Pooling
+
