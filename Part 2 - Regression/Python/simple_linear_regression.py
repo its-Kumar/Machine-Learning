745cea3ed5ad45ba9d3694686df28e5c5f1536e5
Kumar shanu <kumarshanu1009@gmail.com>
2020-07-30 13:29:46 +0530

    Added MLUsecases, NLP, DeepLearning
     On branch master
     Changes committed:
    	modified:   ML Usecases/Breast_Cancer.ipynb
    	new file:   ML Usecases/Titanic Analysis/1_Titanic_data_cleaner.ipynb
    	new file:   ML Usecases/Titanic Analysis/2_Titanic_survival.ipynb
    	new file:   ML Usecases/boston.ipynb
    	new file:   ML Usecases/diabetes.ipynb
    	new file:   ML Usecases/iris.ipynb
    	new file:   ML Usecases/wine.ipynb
    	deleted:    Part 2 - Regression/Python/0_Regression_Template.ipynb~Updated upstream
    	deleted:    Part 2 - Regression/Python/regression_template.py
    	modified:   Part 2 - Regression/Python/simple_linear_regression.py
    	deleted:    Part 4 - Clustering/Python/Hierarchical_clustering.py
    	deleted:    Part 4 - Clustering/Python/K-means_clustering.py
    	renamed:    BERT_NPL.ipynb -> Part 7 - Natural Language Processing/BERT_NPL.ipynb
    	deleted:    Part 7 - Natural Language Processing/NPL_with_DT.py
    	deleted:    Part 7 - Natural Language Processing/NPL_with_KNN.py
    	deleted:    Part 7 - Natural Language Processing/NPL_with_NB.py
    	deleted:    Part 7 - Natural Language Processing/NPL_with_SVM.py
    	deleted:    Part 7 - Natural Language Processing/Natural Language Processing.py
    	renamed:    ANN_regression.ipynb -> Part 8 - Deep Learning/ANN_regression.ipynb
    	modified:   Part 8 - Deep Learning/CNN.ipynb
    	new file:   Part 8 - Deep Learning/dataset/single_prediction/img1.jpg
    	new file:   Part 8 - Deep Learning/dataset/single_prediction/img2.jpg

--- Part 2 - Regression/Python/simple_linear_regression.py
+++ Part 2 - Regression/Python/simple_linear_regression.py
@@ -12,8 +12,10 @@ y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, 
-                                                    random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+     test_size = 1/3,
+     random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
