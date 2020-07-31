db09f2c3cc004621fa4be87c30d71c521575fc51
Kumar shanu <kumarshanu1009@gmail.com>
2020-07-31 11:13:50 +0530

    Added Dimensionality Reduction and update Datapreprocessing

--- Part 1 - Data Preprocessing/Python/data_preprocessing.py
+++ Part 1 - Data Preprocessing/Python/data_preprocessing.py
@@ -12,12 +12,16 @@ y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = Imputer(
+    missing_values='NaN',
+    strategy='mean',
+    axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
-# Encoding the Independent Variable
+
+## Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
--- Part 1 - Data Preprocessing/R/data_preprocessing.R
+++ Part 1 - Data Preprocessing/R/data_preprocessing.R
@@ -4,32 +4,44 @@
 dataset = read.csv("data.csv")
 
 # Taking care of missing data
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age,FUN = function(x) mean(x,na.rm =TRUE)),
-                     dataset$Age)
+dataset$Age = ifelse(
+    is.na(dataset$Age),
+    ave(dataset$Age,
+        FUN = function(x) mean(x, na.rm = TRUE)),
+    dataset$Age)
 
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                        ave(dataset$Salary,FUN = function(x) mean(x,na.rm =TRUE)),
-                        dataset$Salary)
+dataset$Salary = ifelse(
+    is.na(dataset$Salary),
+    ave(dataset$Salary,
+        FUN = function(x) mean(x, na.rm = TRUE)),
+    dataset$Salary)
 
 # Encoding categorical data
-dataset$Country = factor(dataset$Country,
-                         levels= c('France','Spain','Germany'),
-                         labels = c(1,2,3))
+dataset$Country = factor(
+    dataset$Country,
+    levels = c('France','Spain', 'Germany'),
+    labels = c(1, 2, 3))
 
-dataset$Purchased = factor(dataset$Purchased,
-                           levels = c('No','Yes'),
-                           labels = c(0,1))
+dataset$Purchased = factor(
+    dataset$Purchased,
+    levels = c('No', 'Yes'),
+    labels = c(0, 1))
 
 # Spliting the dataset into trainingset and testset
 #install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.8)
-training_set = subset(dataset,split ==TRUE)
-test_set = subset(dataset,split ==FALSE)
+split = sample.split(
+    dataset$Purchased,
+    SplitRatio = 0.8)
+training_set = subset(
+    dataset,
+    split == TRUE)
+test_set = subset(
+    dataset,
+    split == FALSE)
 
 
 #feature scaling
-training_set[,2:3] = scale(training_set[,2:3])
-test_set[,2:3] = scale(test_set[,2:3])
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
